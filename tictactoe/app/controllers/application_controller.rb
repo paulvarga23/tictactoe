d6e0c445a21fc1a85c4afbf7beaf7553d527af14
paulvarga23 <paul_varga@me.com>
2014-05-01 16:38:28 +0100

    update bug fixing

--- tictactoe/app/controllers/application_controller.rb
+++ tictactoe/app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
   def current_user
+    begin
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    
+    rescue ActiveRecord::RecordNotFound
+      session.delete :user_id
+      nil
+
+    end
   end
 
   def logged_in?
